9b3f6d5b409367dadc24fb181d036f4e9525e94b
Ilya <illia.gide@gmail.com>
2020-03-06 22:42:23 +0200

    added Catch2

--- team-algorithms-oop/tests/Tests.cpp
+++ team-algorithms-oop/tests/Tests.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "../deps/Catch2/single_include/catch2/catch.hpp"
+#include "catch.hpp"
 
 #include "../src/SplayTree/SplayTree.hpp"
 
--- team-algorithms-oop/tests/main.cpp
+++ team-algorithms-oop/tests/main.cpp
@@ -1,2 +1,2 @@
 #define CATCH_CONFIG_MAIN
-#include "../deps/Catch2/single_include/catch2/catch.hpp"
+#include "catch.hpp"
